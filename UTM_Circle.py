309e55fdffe7dcea19375a36871ff3eb225c9954
thijsrinkel <161346408+thijsrinkel@users.noreply.github.com>
2025-05-01 18:04:15 +0200

    Update UTM_Circle.py

--- UTM_Circle.py
+++ UTM_Circle.py
@@ -38,8 +38,8 @@ def generate_circle_from_utm(easting, northing, utm_zone=31, radius_m=50, num_po
     return df, round(center_lat, 10), round(center_lon, 10)
 
 # --- Streamlit App ---
-st.image("tr-offshore-survey-logo-yellow-transparent-bg.png", width=150)
 st.title("UTM Point to WGS84 Circles")
+st.image("tr-offshore-survey-logo-yellow-transparent-bg.png", width=150)
 
 st.markdown(
     "Paste UTM coordinates (Easting, Northing) below, one pair per line, comma- or space-separated.  \n"
